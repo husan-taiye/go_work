d193a6089f0eeb0d016293a754eb6a4b21c25756
qilu <qilu@xuetangx.com>
2023-08-24 21:52:17 +0800

    week 1 work

--- week_1_work/slice/delete.go
+++ week_1_work/slice/delete.go
@@ -2,8 +2,9 @@ package slice
 
 import "errors"
 
-// 第一第三条实现了 第四条要求缩容暂时没实现
-func Pop[T any](S []T, idx int) ([]T, error) {
+// Delete 第一第三条实现了 第四条要求缩容暂时没实现
+// 第二条都是在原地操作
+func Delete[T any](S []T, idx int) ([]T, error) {
 	l := len(S)
 	if idx >= l {
 		return S, errors.New("传入的下标必须小于切片的长度")
